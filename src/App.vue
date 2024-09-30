85e7788cb3a2d35ad8259bb10480669ca6a2a826
AaronAlons <159506374+AaronAlons@users.noreply.github.com>
2024-09-30 03:37:51 +0000

    🦖FEAT: add app.vue add an button,checkbox and text area

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,14 @@ const items = ref([
     {id:'6',label:'1 pc gamer'}
 ]);
 const newItem =ref('');
+const newItemHighPriority = ref(false);
+/*Helados
+const iceCreamFlavors = ref([]);*/
+/*Enlazado con checkboxes
+const newItemHighPriority = ref(false);*/
+/*radio buttons
 const newItemPriority= ref('low');
+*/
 </script>
 
 <template>
@@ -19,10 +26,11 @@ const newItemPriority= ref('low');
     <i class="material-icons shopping-cart-icon">local_mall</i>
     {{ header }}
 </h1> 
-<input v-model="newItem"
+<!--<input v-model="newItem"
 type="text" 
 placeholder="Agregar articulo">
-<!--Radio Buttons-->
+-->
+<!--Radio Buttons
 <label>
     <input type="radio" value="low" v-model="newItemPriority">
     Bajo
@@ -32,6 +40,65 @@ placeholder="Agregar articulo">
     Alto
 </label>
 {{ newItemPriority =='low'?'🦖':'❤️‍🔥'}}
+-->
+<!--Enlazado con checkboxes
+Radio Buttons 
+<label><input type="checkbox" v-model="newItemHighPriority">Alta Prioridad</label> <br>
+{{ newItemHighPriority }}-->
+<!-- Helados
+<label>
+		<input type="checkbox" v-model="iceCreamFlavors" value="vanilla">
+		Vanilla
+	</label>
+  <label>
+		<input type="checkbox" v-model="iceCreamFlavors" value="chocolate">
+		Chocolate
+	</label>
+  <label>
+		<input type="checkbox" v-model="iceCreamFlavors" value="strawnerry">
+		Strawberry
+	</label>
+  {{iceCreamFlavors}}
+-->
+<!--agregar sin guardar
+<input 
+      type="text" 
+      placeholder="Add Item" 
+      v-on:keyup.enter="items.push({id: items.length + 1, label: newItem})" 
+      v-model.trim="newItem">
+ Caja de seleccion de prioridad 
+<label>
+    <input type="checkbox" v-model="newItemHighPriority">
+    High Priority
+  </label>
+   Boton 
+<button 
+  class="btn btn-primary" 
+  v-on:click="items.push({id: items.length + 1, label: newItem})">
+  Salvar Articulo
+</button>
+-->
+
+<!-- Agrupando en un div las entradas -->
+<form v-on:submit.prevent="items.push({ id: items.length + 1, label: newItem })" class="add-item fomr">
+    <!-- entrada de texto -->
+    <input
+      v-model.trim="newItem"
+      type="text"
+      placeholder="Add Item"
+    />
+    <!-- Caja de seleccion de prioridad -->
+    <label>
+      <input type="checkbox" v-model="newItemHighPriority" />
+      High Priority
+    </label>
+    <!-- Boton -->
+    <button
+      class="btn btn-primary"
+    >
+      Save Item
+    </button>
+  </form>
 <!--ul>li*3 es el lenguaje emet a usar-->
 <ul>
     <li v-for="item in items" :key="item.id"> 🦖🦖 {{ item.label }}</li>
@@ -41,5 +108,8 @@ placeholder="Agregar articulo">
 <style scoped>
 .shopping-cart-icon{
     font-size: 2rem;
+}
+button{
+    background-color: blue;
 }
 </style>
