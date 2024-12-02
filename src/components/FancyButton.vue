854894184f1edad7e882ba2deeb965c7eb2b6b5e
aaronalonso <aaronalonso200216@gmail.com>
2024-12-01 22:45:35 -0600

    🤔FEAT: First Slot

--- src/components/FancyButton.vue
+++ src/components/FancyButton.vue
@@ -1,11 +1,18 @@
 <template>
-    <button>Hazme Clic!</button>
+    <button> 
+      <slot></slot> 
+    </button>
   </template>
-  
   <script setup>
   </script>
   
   <style scoped>
-  
+  .content{
+  text-align: center;
+}
+button{
+  width: 12rem;
+  margin: 30px auto;
+  font-size: 2rem;
+} 
   </style>
-  
